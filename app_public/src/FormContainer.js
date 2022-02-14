35d38fcc85b4ca9a1702ca43f1223b1a8a922c24
kkeroo <jasko10.kerec@gmail.com>
2022-02-14 18:46:58 +0100

    Dodana slika za ozadje.

---
+++ app_public/src/FormContainer.js
@@ -0,0 +1,16 @@
+import './FormContainer.css'
+import Container from 'react-bootstrap/Container';
+
+const FormContainer = () => {
+    return (
+        <div className="FormContainer">
+            <Container fluid className="summary-block">
+                <Container>
+                    
+                </Container>
+            </Container>
+        </div>
+    );
+}
+
+export default FormContainer;
